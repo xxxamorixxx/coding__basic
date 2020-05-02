8d4b472af5aedaf52d830dbd758eae184e6accce
xxxamorixxx <xxxamorixxx@gmail.com>
2020-05-03 04:52:05 +0900

    aa

---
+++ library/js/script.js
@@ -0,0 +1,45 @@
+//画像差し替え
+$(document).ready(function(){
+     $('#menunv ul li a img.pc').hover(function(){
+        $(this).attr('src', $(this).attr('src').replace('_off', '_on'));
+          }, function(){
+             if (!$(this).hasClass('currentPage')) {
+             $(this).attr('src', $(this).attr('src').replace('_on', '_off'));
+        }
+   });
+});
+
+
+//ページTOPへ
+$(function () {
+    var topBtn = $('#page-top');
+    topBtn.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
+
+
+//スムーススクロール
+$(function () {
+  $('a[href^="#"]').click(function () {
+      var speed = 400;
+      var href = $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top;
+      $('body,html').animate({
+          scrollTop: position
+      }, speed, 'swing');
+      return false;
+  });
+});
